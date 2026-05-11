8b13aaa2fc9765269fdbf0536a39b575192fafc0
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2026-05-12 02:04:48 +0700

    upda

---
+++ lang/id/pagination.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa Penomoran Halaman (Pagination)
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan oleh pustaka paginator untuk membangun
+    | tautan penomoran halaman yang sederhana. Anda bebas untuk mengubahnya
+    | menjadi apa pun yang Anda inginkan untuk menyesuaikan tampilan Anda
+    | agar lebih cocok dengan aplikasi Anda.
+    |
+    */
+
+    'previous' => '&laquo; Sebelumnya',
+    'next' => 'Berikutnya &raquo;',
+
+];
---
+++ lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa Atur Ulang Kata Sandi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut adalah baris default yang cocok dengan alasan
+    | yang diberikan oleh broker kata sandi untuk upaya pembaruan kata sandi
+    | hasil seperti kegagalan karena kata sandi / token atur ulang yang tidak valid.
+    |
+    */
+
+    'reset' => 'Kata sandi Anda telah diatur ulang.',
+    'sent' => 'Kami telah mengirimkan tautan atur ulang kata sandi ke surel Anda.',
+    'throttled' => 'Harap tunggu sebelum mencoba lagi.',
+    'token' => 'Token atur ulang kata sandi ini tidak valid.',
+    'user' => "Kami tidak dapat menemukan pengguna dengan alamat surel tersebut.",
+
+];
